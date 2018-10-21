6e2237f18969e6a0b23d91c08c0445460dacf3b3
Gonçalo <goncalo.gabriel@gmail.com>
2018-10-21 15:41:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/senhas')
+def index():
+    return render_template('senha.html', num=1)
+
 @app.route('/senhas/<num>')
 def numer(num):
     return render_template('senha.html', num=num)
-    
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
