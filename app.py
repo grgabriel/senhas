c8ae39eec40487f4895a88fd9c5f51d55c27610a
Gonçalo <goncalo.gabriel@gmail.com>
2018-10-21 15:50:04 +0100

    update 0.0000002

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ def index():
 
 @app.route('/senhas/<num>')
 def numero(num):
-    return render_template('senhas.html', num=num)
+    next = num + 1
+    return render_template('senhas.html', num=num, next=next)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
