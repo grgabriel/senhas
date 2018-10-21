7f029a5a7cbf3c32fa2d805dd132ecce5a327ed1
Gonçalo <goncalo.gabriel@gmail.com>
2018-10-21 15:36:33 +0100

    Senhas v0.1

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/senhas/<num>')
+def numer(num):
+    return render_template('senha.html', num=num)
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
