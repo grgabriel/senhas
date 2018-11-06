c2a1a4726956d8e68e9a10d5d9c19b3db31fd4be
Gonçalo <goncalo.gabriel@gmail.com>
2018-11-06 14:45:26 +0000

    Update lin_funcs.py

--- lin_funcs.py
+++ lin_funcs.py
@@ -1,11 +1,19 @@
-from escpos.connections import getUSBPrinter
+import subprocess
 
 def imprimir(n):
-    print("PRINTING {}".format(n))
-    printer = getUSBPrinter()(idVendor=0x04f9,
-			      idProduct=0x203a,
-                              inputEndPoint=0x81,
-                              outputEndPoint=0x02)
-    printer.text("Test")
-    printer.lf()
+    lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
+    lpr.stdin.write("10".encode('utf-8'))
+    lpr.terminate()
+    
+
+#from escpos.connections import getUSBPrinter
+#
+#def imprimir(n):
+#    print("PRINTING {}".format(n))
+#    printer = getUSBPrinter()(idVendor=0x04f9,
+#			      idProduct=0x203a,
+#                              inputEndPoint=0x81,
+#                              outputEndPoint=0x02)
+#    printer.text("Test")
+#    printer.lf()
 
